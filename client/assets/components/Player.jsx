3ec99854574de4922c5754daff88ffef4014a5d8
MrRazorez <viantosa1999@gmail.com>
2022-08-13 14:52:17 +0700

    first

--- client/assets/components/Player.jsx
+++ client/assets/components/Player.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet } from 'react-native';
+import { Dimensions, StyleSheet } from 'react-native';
 import { Video } from 'expo-av';
+import * as ScreenOrientation from 'expo-screen-orientation';
+import CryptoJS from 'crypto-js';
 import axios from 'axios';
 
 export default function Media() {
@@ -8,14 +10,23 @@ export default function Media() {
 
     const callAPI = async () => {
       try {
-        const res = await axios.get('http://192.168.180.69:3000/');
-        setDataVideo(res.data.video);
+        const res = await axios.get('http://192.168.64.226:3000/');
+        setDataVideo(CryptoJS.AES.decrypt(res.data.video, "test123").toString(CryptoJS.enc.Utf8));
       }catch(error) {
         console.log(error);
       }
     }
 
     useEffect(() => {callAPI()}, []);
+
+    const setOrientation = () => {
+      if (Dimensions.get('window').height > Dimensions.get('window').width) {
+        ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.LANDSCAPE);
+      }
+      else {
+        ScreenOrientation.lockAsync(ScreenOrientation.OrientationLock.PORTRAIT);
+      }
+    }
 
     const video = React.useRef(null);
     const [status, setStatus] = React.useState({});
@@ -27,7 +38,9 @@ export default function Media() {
             uri: dataVideo,
             }}
             useNativeControls
-            resizeMode="contain"
+            resizeMode="cover"
+            shouldPlay
+            onFullscreenUpdate={setOrientation}
             onPlaybackStatusUpdate={status => setStatus(() => status)}
         />
     );
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+var aes = require('crypto-js/aes');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   const path = req.protocol+'://'+req.headers.host+req.originalUrl+'videos/video.mp4';
   res.json({
-    video: path
+    video: aes.encrypt(path, "test123").toString()
   });
 });
 
